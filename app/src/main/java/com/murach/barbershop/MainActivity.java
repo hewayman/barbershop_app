277133a45bfa599a4ffddd44a5bf274225630e8b
hewayman <hewayman@gmail.com>
2021-03-12 18:39:08 -0500

    first commit

---
+++ app/src/main/java/com/murach/barbershop/MainActivity.java
@@ -0,0 +1,37 @@
+package com.murach.barbershop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        // set listener for login button
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHome();
+            }
+        });
+    }
+
+    // continues to home page
+    public void openHome() {
+        Intent intent = new Intent(this, UserHome.class);
+        startActivity(intent);
+    }
+}
