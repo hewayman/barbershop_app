6bed951be2da6184c890b2b737c60f98dafdf840
hewayman <hewayman@gmail.com>
2021-04-01 14:23:38 -0400

    adding create account activity

--- app/src/main/java/com/murach/barbershop/MainActivity.java
+++ app/src/main/java/com/murach/barbershop/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button loginBtn;
+    private TextView registerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addListenerOnButton();
+        addListenerForRegistration();
     }
 
     public void addListenerOnButton() {
@@ -29,9 +32,26 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void addListenerForRegistration() {
+        // set listener for registration text view
+        registerTextView = (TextView) findViewById(R.id.registerTextView);
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegistration();
+            }
+        });
+    }
+
     // continues to home page
     public void openHome() {
         Intent intent = new Intent(this, UserHomeActivity.class);
+        startActivity(intent);
+    }
+
+    // continues to registration page
+    public void openRegistration() {
+        Intent intent = new Intent(this, UserRegistration.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/murach/barbershop/UserRegistration.java
@@ -0,0 +1,19 @@
+package com.murach.barbershop;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // sets layout to user registration page
+        setContentView(R.layout.register_home);
+
+    }
+
+}
