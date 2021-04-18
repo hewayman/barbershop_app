8c2a3c4903443da416540cffce3ddd2b6978207b
hewayman <hewayman@gmail.com>
2021-04-18 16:10:52 -0400

    adding buttons to confirmation page

--- app/src/main/java/com/murach/barbershop/ConfirmationActivity.java
+++ app/src/main/java/com/murach/barbershop/ConfirmationActivity.java
@@ -1,6 +1,9 @@
 package com.murach.barbershop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ConfirmationActivity extends AppCompatActivity {
 
     private TextView dateConfirmation;
+    private Button backBtn;
+    private Button bookBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,31 @@ public class ConfirmationActivity extends AppCompatActivity {
         dateConfirmation = (TextView) findViewById(R.id.dateConfirmation);
         dateConfirmation.setText("Test");
 
+        backBtn = (Button) findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenCalendar();
+            }
+        });
+
+        bookBtn = (Button) findViewById(R.id.bookBtn);
+        bookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoHome();
+            }
+        });
+
+    }
+
+    public void OpenCalendar() {
+        Intent intent = new Intent(this, CalendarActivity.class);
+        startActivity(intent);
+    }
+
+    public void GoHome() {
+        Intent intent = new Intent(this, UserHomeActivity.class);
+        startActivity(intent);
     }
 }
